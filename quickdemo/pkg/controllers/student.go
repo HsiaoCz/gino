60512de065535afb5a1112d3c61aeedb32dd3e44
HsiaoCz <984274788@qq.com>
2023-07-14 18:37:35 +0800

    quick demo

--- quickdemo/pkg/controllers/student.go
+++ quickdemo/pkg/controllers/student.go
@@ -1,8 +1,44 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
 
-func CreateStudent(w http.ResponseWriter, r *http.Request)    {}
+	"github.com/HsiaoCz/gino/quickdemo/pkg/models"
+	"github.com/HsiaoCz/gino/quickdemo/pkg/utils"
+)
+
+func CreateStudent(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.FormValue("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("请输入数字学号"))
+		return
+	}
+	chinese, err := strconv.ParseFloat(r.FormValue("chinese"), 32)
+	if err != nil {
+		utils.ResponseString(w, http.StatusOK, err.Error())
+		return
+	}
+	math, err := strconv.ParseFloat(r.FormValue("math"), 32)
+	if err != nil {
+		utils.ResponseString(w, http.StatusOK, err.Error())
+		return
+	}
+	english, err := strconv.ParseFloat(r.FormValue("english"), 32)
+	if err != nil {
+		utils.ResponseString(w, http.StatusOK, err.Error())
+	}
+	student := &models.Student{
+		ID:      id,
+		Name:    r.FormValue("name"),
+		Class:   r.FormValue("class"),
+		Chinese: float32(chinese),
+		Math:    float32(math),
+		English: float32(english),
+	}
+	utils.ResponseJSON(w, http.StatusOK, student)
+}
 func DeleteStudent(w http.ResponseWriter, r *http.Request)    {}
 func UpdateStudent(w http.ResponseWriter, r *http.Request)    {}
 func GetSingleStudent(w http.ResponseWriter, r *http.Request) {}
--- quickdemo/pkg/utils/utils.go
+++ quickdemo/pkg/utils/utils.go
@@ -1,1 +1,19 @@
 package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+func ResponseJSON(w http.ResponseWriter, code int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(v)
+}
+
+func ResponseString(w http.ResponseWriter, code int, str string) {
+	w.WriteHeader(code)
+	w.Write([]byte(str))
+}
